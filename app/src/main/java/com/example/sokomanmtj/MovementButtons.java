9922e04ab1118b80ddc4a4e975d7c01837e42c05
Red-system <thibaut.jouet64@gmail.com>
2022-05-05 10:52:25 +0200

    :construction: Grid and direction buttons

---
+++ app/src/main/java/com/example/sokomanmtj/MovementButtons.java
@@ -0,0 +1,14 @@
+package com.example.sokomanmtj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovementButtons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movement_buttons);
+    }
+}
